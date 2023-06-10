0af2854135257fffa53f73d86f5c12a44d083b4e
SREESANKAR-P <sreesankarputhumana@gmail.com>
2023-06-10 17:03:13 +0530

    updated middileware

---
+++ middleware/multer.js
@@ -0,0 +1,50 @@
+const multer = require('multer')
+const sharp = require('sharp');
+const fs = require('fs');
+const path = require('path')
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, '../public/images'));
+    },
+    filename: function (req, file, cb) {
+        const name = Date.now() + '-' + file.originalname;
+        cb(null, name);
+    }
+});
+
+const imageFilter = function (req, file, cb) {
+    // accept image only  
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+        req.fileValidationError = 'Only image files are allowed!';
+        return cb(null, false);
+    }
+    cb(null, true);
+};
+
+
+const store = multer({ storage: storage, fileFilter: imageFilter });
+
+
+// image crop  
+// const sharpImage = (req, res, next) => {
+//     req.files.forEach(file => {
+//         const inputBuffer = fs.readFileSync(file.path);
+//         sharp(inputBuffer)
+//             .resize({
+//                 width: 400,
+//                 height: 400,
+//                 fit: cover
+//             })
+//             .toFile(file.path, (err) => {
+//                 if (err) throw err;
+//             });
+//     });
+//     next();
+// };
+
+module.exports = { 
+    store, 
+    // sharpImage
+ }
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -3,44 +3,24 @@ const express = require("express");
 const adminRoute = express();
 
 const adminController = require("../controllers/admin/adminController");
-const userController = require('../controllers/user/userController');
+// const userController = require('../controllers/user/userController');
 const categoryController = require('../controllers/categoryController');
 const productController = require('../controllers/productController');
 const multer = require('multer');
 const session = require('express-session');
-const config = require('../config/config');
+// const config = require('../config/config');
 const adminauth = require('../middleware/adminauth');
 const path = require('path');
-const productModel = require("../model/productModel");
+// const productModel = require("../model/productModel");
 const app = express();
+const store = require('../middleware/multer');
+
 
 adminRoute.set('view engine', 'hbs')
 adminRoute.set('views', './views/admin')
 
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '../public/images'));
-  },
-  filename: function (req, file, cb) {
-    const name = Date.now() + '-' + file.originalname;
-    cb(null, name);
-  }
-});
-
-const imageFilter = function (req, file, cb) {
-  // accept image only  
-  if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-    req.fileValidationError = 'Only image files are allowed!';
-    return cb(null, false);
-  }
-  cb(null, true);
-};
-
-
-const upload = multer({ storage: storage, fileFilter: imageFilter });
-
-app.post('/updateproduct', upload.array('image'), function (req, res) {
+app.post('/updateproduct',store.store .array('image'), function (req, res) {
   if (req.fileValidationError) {
     return res.status(400).json({ error: req.fileValidationError });
   }
@@ -62,25 +42,26 @@ adminRoute.get('/dashboard', adminauth.isLoginAdmin, adminController.loadDashboa
 adminRoute.get('/users', adminauth.isLoginAdmin, adminController.userList)
 adminRoute.get("/logout", adminauth.isLoginAdmin, adminController.adminLogout);
 
+//block or unblock user
 adminRoute.get('/blockUser', adminauth.isLoginAdmin, adminController.blockUser)
 adminRoute.get('/unblockUser', adminauth.isLoginAdmin, adminController.unblockUser)
 
 //category
 adminRoute.get('/category', adminauth.isLoginAdmin, categoryController.loadCategory);
 adminRoute.get('/addcategory', adminauth.isLoginAdmin, categoryController.loadAddCategory)
-adminRoute.post('/addcategory', upload.single('image'), categoryController.insertCategory)
+adminRoute.post('/addcategory', store.store.single('image'), categoryController.insertCategory)
 adminRoute.get('/editCategory', adminauth.isLoginAdmin, categoryController.loadEditCategory)
-adminRoute.post('/editCategory',upload.single('image') ,adminauth.isLoginAdmin, categoryController.updatecategory)
+adminRoute.post('/editCategory',store.store.single('image') ,adminauth.isLoginAdmin, categoryController.updatecategory)
 adminRoute.post('/delete', adminauth.isLoginAdmin, categoryController.deleteCategory);
 
 //product
 adminRoute.get('/product', adminauth.isLoginAdmin, productController.loadProduct);
 adminRoute.get('/addproduct', adminauth.isLoginAdmin, productController.loadAddProduct);
-adminRoute.post('/addproduct', adminauth.isLoginAdmin, upload.array('image', 10), productController.addProduct);
+adminRoute.post('/addproduct', adminauth.isLoginAdmin, store.store.array('image', 10), productController.addProduct);
 adminRoute.get('/updateProduct/:id', adminauth.isLoginAdmin, productController.loadUpdateProduct);
-adminRoute.post('/updateProduct/:id', adminauth.isLoginAdmin, upload.array('image', 10), productController.updateProduct)
+adminRoute.post('/updateProduct/:id', adminauth.isLoginAdmin, store.store.array('image', 10), productController.updateProduct)
 
-//block
+//block product
 adminRoute.get('/blockproduct',adminauth.isLoginAdmin,productController.blockProduct);
 adminRoute.get('/unblockproduct',adminauth.isLoginAdmin,productController.unBlockProduct);
 
