904fcf6e506a24b36306097315e63470462f2ccc
SREESANKAR-P <sreesankarputhumana@gmail.com>
2023-07-06 17:19:03 +0530

    installed pdfkit

--- controllers/admin/dashboardController.js
+++ controllers/admin/dashboardController.js
@@ -1,3 +1,5 @@
 const moment = require('moment');
 const Sale = require('../../model/orderModel');
 const Order = require('../../model/orderModel');
+const PDFDocument = require('pdfkit');
+const hbs = require('hbs');
