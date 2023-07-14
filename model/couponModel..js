b1ca13242c2c702c639274af1419711f866594df
SREESANKAR-P <sreesankarputhumana@gmail.com>
2023-07-14 23:45:25 +0530

    coupon model added

---
+++ model/couponModel..js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const couponSchema = new mongoose.Schema({
+
+    code: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    discount: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 100
+    },
+
+    expiryDate: {
+        type: Date,
+        required: true
+    },
+
+    status: {
+        type: Boolean,
+        default: true
+    },
+
+    usedBy: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+
+});
+
+module.exports = mongoose.model('Coupon', couponSchema);
