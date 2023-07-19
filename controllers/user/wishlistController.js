50c68d0d2449255b80809bf4534e7bb871fd7dfe
SREESANKAR-P <sreesankarputhumana@gmail.com>
2023-07-19 23:54:31 +0530

    updatated

--- controllers/user/wishlistController.js
+++ controllers/user/wishlistController.js
@@ -4,95 +4,69 @@ const Product = require('../../model/productModel');
 
 
 const loadWishlist = async (req, res) => {
-    try {
-        const userData = req.session.userdata;
-        const userId = userData._id;
-        console.log(userId,"userIddddddd from wishlist")
-        const user = await User.findById(userId).populate('wishlist');
-        const wishItem = user.wishlist;
-        console.log(user,"userr from wishlist")
-        console.log(wishItem,"wishitem from wishlist");
-        res.render('wishlist',{userData,wishItem})
-    } catch (error) {
-        console.log(error.message);
-    }
+  try {
+    const userData = req.session.userdata;
+    const userId = userData._id;
+    const user = await User.findById(userId).populate('wishlist');
+    const wishItem = user.wishlist;
+    res.render('wishlist', { userData, wishItem });
+
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-// const addToWishlist = async (req, res) => {
-//     try {
-//         const userData = req.session.userdata;
-//         const userId  = userData._id
-//         const proId  = req.query.id
-
-//         const user = await User.findById(userId);
-//         const itemExists = user.wishlist.includes(proId);
-//         if (!itemExists) {
-//             await User.updateOne({ _id: userId }, { $push: { wishlist: proId }})
-//             await Product.updateOne({ _id: proId }, { isWishlisted : true })
-//             res.json({ 
-//               message: 'Added',
-              
-//            })
-//           } else {
-//             res.json({
-//                message : 'Exist',
-//            })
-//           }
-//     } catch (error) {
-//         console.log(error.message)
-//     }
-// }
 const addToWishlist = async (req, res) => {
-    try {
-        const userData = req.session.userdata;
-        const userId = userData._id;
-        const proId = req.query.id;
+  try {
+    const userData = req.session.userdata;
+    const userId = userData._id;
+    const proId = req.query.id;
 
-        const user = await User.findById(userId);
-        const itemExists = user.wishlist.includes(proId);
+    const user = await User.findById(userId);
+    const itemExists = user.wishlist.includes(proId);
 
-        if (!itemExists) {
-            await User.updateOne({ _id: userId }, { $push: { wishlist: proId } });
-            await Product.updateOne({ _id: proId }, { isWishlisted: true });
-            res.json({ message: 'Added' });
-        } else {
-            res.json({ message: 'Exist' });
-        }
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: 'Server Error' });
+    if (!itemExists) {
+      await User.updateOne({ _id: userId }, { $push: { wishlist: proId } });
+      await Product.updateOne({ _id: proId }, { isWishlisted: true });
+      res.json({ message: 'Added' });
+    } else {
+      res.json({ message: 'Exist' });
     }
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: 'Server Error' });
+  }
 };
 
-const removeFromWishList = async(req, res) => {
-    try {
-      const userData = req.session.user
-      const userId   = userData._id
-      const proId    = req.query.id
-  
-      const user      = await User.findById(userId)
-      const itemIndex = user.wishlist.indexOf(proId)
-  
-      if (itemIndex >= 0) {
-        await User.updateOne({ _id: userId }, { $pull: { wishlist: proId }})
-        await Product.updateOne({ _id: proId }, { isWishlisted: false })
-        res.json({
-          message: 'Item removed from wishlist!',
-          status: true
-        })
-      } else {
-        res.json({ message: 'Item not found in wishlist!' })
-      }
-  
-    } catch (error) {
-      console.log(error);
+const removeFromWishList = async (req, res) => {
+  try {
+    const userData = req.session.userdata
+    const userId = userData._id
+    const proId = req.query.id
+
+    const user = await User.findById(userId)
+    const itemIndex = user.wishlist.indexOf(proId)
+
+    if (itemIndex >= 0) {
+      await User.updateOne({ _id: userId }, { $pull: { wishlist: proId } })
+      await Product.updateOne({ _id: proId }, { isWishlisted: false })
+      res.json({
+        message: 'Item removed from wishlist!',
+        status: true
+      })
+    } else {
+      res.json({ message: 'Item not found in wishlist!' })
     }
+
+  } catch (error) {
+    console.log(error);
   }
-  
+}
+
 
 
 module.exports = {
-    loadWishlist,
-    addToWishlist,
-    removeFromWishList
+  loadWishlist,
+  addToWishlist,
+  removeFromWishList
 }
